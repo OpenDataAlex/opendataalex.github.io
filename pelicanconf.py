7ac2dd932e41c2a1b588e3fcda0f56ad273edd8d
Alex Meadows <alex.meadows@bluefiredatasolutions.com>
2020-10-11 16:21:48 -0400

    Initial Pelican commit to content branch

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
 
-AUTHOR = 'opendataalex'
+AUTHOR = 'Alex Meadows'
 SITENAME = "OpenDataAlex's Blog"
 SITEURL = ''
 
@@ -19,16 +19,29 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/OpenDataAlex'),
+          ('LinkedIn', 'https://www.linkedin.com/in/alexmeadows/'),
+          ('SllideShare', 'https://www.slideshare.net/dba_alex'),)
 
-DEFAULT_PAGINATION = False
+DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = "/home/ameadows/PycharmProjects/pelican-twitchy"
+
+EXPAND_LATEST_ON_INDEX = True
+OPEN_GRAPH = False
+OPEN_GRAPH_IMAGE = False
+SHARE = True
+
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_CATEGORIES_ON_MENU = True
+DISPLAY_TAGS_ON_MENU = True
+
+CC_LICENSE = 'CC-BY-SA-ND-NC'
